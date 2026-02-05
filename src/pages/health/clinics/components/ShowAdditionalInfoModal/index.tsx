ac371fea439821f2f2a11544fb5178b30aca8263
IliaRy <i.ryzhov@modesco.ru>
2026-02-05 21:36:43 +0400

    Save

--- src/pages/health/clinics/components/ShowAdditionalInfoModal/index.tsx
+++ src/pages/health/clinics/components/ShowAdditionalInfoModal/index.tsx
@@ -8,6 +8,7 @@ import EditIcoSmall from "../../../../../assets/icons/editIcoSmall.svg";
 import { IClinic } from "../../../../../interfaces/analysis";
 
 import './index.scss';
+import { BtnSize } from "../../../../../types/components";
 
 const ShowAdditionalInfoModal: React.FC<{
   closeModal: (isClose: null) => void,
@@ -50,17 +51,42 @@ const ShowAdditionalInfoModal: React.FC<{
                 )
                 : <div>No phones</div>
               }
+              <div>
+                <Button
+                  size={BtnSize.largeBtn}
+                  title={'Add phone'}
+                  onClick={() => console.log('Add')}
+                />
+              </div>
             </div>
           ) 
           : (
-          <div className="showInfo__form__info">
-            <div className="showInfo__form__info__item">
+            <div className="showInfo__form__info">
+              <div className="showInfo__form__info__header">
+                <p className="showInfo__form__info__header_val">Title</p>
+                <p className="showInfo__form__info__header_val">Number</p>
+                <p className="showInfo__form__info__header_val">Is main number</p>
+              </div>
               {
-                clinic.addresses?.length ? clinic.addresses.map((address) => (<div>{address.title}</div>))
-                : (<div>No address</div>)
+                clinic.addresses?.length ? clinic.addresses.map((address) => (
+                  <div key={address.id} className="showInfo__form__info__item">
+                    <div className="showInfo__form__info__item_val">{address.title}</div>
+                    <div className="showInfo__form__info__item_val">{address.address}</div>
+                    <div className="showInfo__form__info__item_val">{address.is_main ? 'Yes' : 'No'}</div>
+                    <img src={EditIcoSmall} alt="Edit value" className='valuesBox__valuesTable__controlBox__item'/>
+                    <img src={DelIcoSmall} alt="Delete value" onClick={() => console.log(address.id)} className='valuesBox__valuesTable__controlBox__item'/>
+                  </div>
+              ))
+                : <div>No address</div>
               }
+              <div>
+                <Button
+                  size={BtnSize.largeBtn}
+                  title={'Add address'}
+                  onClick={() => console.log('Add')}
+                />
+              </div>
             </div>
-          </div>
           )
 
         }
